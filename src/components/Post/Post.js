4674ae661f9d439ba926366faf61b4c9d8349df9
SANDRA <sandrarehan97@gmail.com>
2021-08-24 19:18:16 +0530

    UI post,feed,tweetbox

---
+++ src/components/Post/Post.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./Post.scss";
+import like from "../../assets/images/icons/like.svg";
+import msg from "../../assets/images/icons/message.svg";
+import share from "../../assets/images/icons/share.svg";
+
+class Post extends Component {
+    render(){
+        return(
+            <div className="post-container">
+                <div className="poster">
+                    <div className="posterimg">
+                       <a href="./"> <img src={this.props.posterprofile} alt="posterimage" className="profile-pic"></img></a>
+                    </div>
+                    <div className="name_desig">
+                        <a href="./"><p className="Name">{this.props.poster}</p></a>
+                        <p className="Desig">{this.props.designation}</p>
+                    </div>
+                </div>
+                <div className="post-content">
+                    <p>{this.props.content}</p>
+                    <p className="hashtags">{this.props.hashtags}</p>
+                    <img src={this.props.image} alt={this.props.content}></img>
+                </div>
+                <div className="post-attributes">
+                    <p>{this.props.likes} Likes</p>
+                    <p>{this.props.comments} Comments</p>
+                </div>
+                <div className="post-activity">
+                    <div>
+                    <img src={like} alt="Like"></img>
+                    </div>
+                    <div>
+                    <img src={msg} alt="Message"></img>
+                    </div>
+                    <div>
+                    <img src={share} alt="Share"></img>
+                    </div>
+                </div>
+                <div className="post-comment">
+                    <img src={this.props.profilepic} alt="Profilepic"></img>
+                    <input type="text" placeholder="Add a comment" className="comment-input-field"></input>
+                    
+                </div>
+                <div className="load-comments">
+                
+                <a href="./">View all comments</a>
+                </div>
+            </div>
+        )
+    }
+}
+export default Post;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
-
+import Tweetbox from "./TweetBox/Tweetbox"
 import "./Home.scss";
 import Card from "../../components/Card/Card";
+import Post from "../../components/Post/Post";
 import Header from "../../components/Header/Header";
+import postImg from "../../assets/images/icons/postImg.png";
+import profilepic from "../../assets/images/icons/UserProfile.png";
 
 class Home extends Component {
   render() {
@@ -16,11 +19,37 @@ class Home extends Component {
         <Header active={"home"} />
 
         <div className="container">
+          <div className="card-cols">
+          <div className="card-col1">
+          <div className="tweetbox-container">
+              <Tweetbox></Tweetbox>
+          </div>
+          <Card>
+          <Post 
+              poster="Name" 
+              posterprofile={profilepic}
+              designation="poster designation" 
+              content="Ut enim ad minim veniam, quis nostrud exercitatioul lam co laboris nisi ut aliquip.
+                      Hashtags   lorem_epsum"
+              hashtags="#Hashtags   #lorem_epsum"
+              image={postImg}
+              likes="3k"
+              comments="1k"
+              profilepic={profilepic}
+          >
+              
+          </Post>
+          </Card>
+          
+          </div>
+          <div className="card-col2">
           <Card>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s
           </Card>
+          </div>
+          </div>
         </div>
       </div>
     );
