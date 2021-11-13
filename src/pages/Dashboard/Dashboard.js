96c8ef812254e065e0108d1ec7b77a5dfe0f9d41
Kemosabe2911 <stevinprince29@gmail.com>
2021-11-13 15:53:02 +0530

    Update dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import "./Dashboard.scss";
 import Header from "../../components/Header/Header";
 import DashJobCard from "../../components/DashJobcard/DashJobcard";
 import clogo from "../../assets/images/icons/company-logo.png";
+import edit from "../../assets/images/icons/edit.svg";
+import del from "../../assets/images/icons/delete-red.svg";
 
 
 class Dashboard extends Component{
@@ -34,6 +36,26 @@ class Dashboard extends Component{
                 date: '14/11/2021',
                 newapp: 20,
                 totalapp: 77
+            }
+        ],
+        applications: [
+            {
+                name: "Mary Jane Watson",
+                position: "ML Intern",
+                msg: "lorem ipsum",
+                remark: "Potencial Candidate"
+            },
+            {
+                name: "Mary Jane Watson",
+                position: "ML Intern",
+                msg: "lorem ipsum",
+                remark: "Potencial Candidate"
+            },
+            {
+                name: "Mary Jane Watson",
+                position: "ML Intern",
+                msg: "lorem ipsum",
+                remark: "Potencial Candidate"
             }
         ]
     }
@@ -67,6 +89,56 @@ class Dashboard extends Component{
                             )}
                             <div className="add-btnContainer">
                                 <buttun className="add-btn">Add New Job Posting</buttun>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="applications">
+                        <h3>Applications</h3>
+                        <div className="tb-container">
+                            <div className="tb-titles">
+                                <div className="name">
+                                    <span className="tb-title">Name</span>
+                                </div>
+                                <div className="pos">
+                                    <span className="tb-title">Position Applying for</span>
+                                </div>
+                                <div className="msg">
+                                    <span className="tb-title">Message from Candidate</span>
+                                </div>
+                                <div className="remarks">
+                                    <span className="tb-title">Remarks</span>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="tb-dataContainer">
+                                {this.state.applications.map((app) =>
+                                    <div className="tb-datasContainer">
+                                        <div className="tb-datas">
+                                            <div className="name">
+                                                <span className="tb-data">{app.name}</span>
+                                            </div>
+                                            <div className="pos">
+                                                <span className="tb-data">{app.position}</span>
+                                                <div className="tb-links">
+                                                    <a href="#">View Profile</a>
+                                                    <a href="#">Resume</a>
+                                                </div>
+                                            </div>
+                                            <div className="msg">
+                                                <span className="tb-data">{app.msg}</span>
+                                            </div>
+                                            <div className="remarks">
+                                                <span className="tb-data">{app.remark}<img src={edit} alt="Edit"></img></span>
+                                                <div className="rem-icons">
+                                                    <img src={del} alt="Delete"></img>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <br/>
+                                        <hr></hr>
+                                        <br/>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
